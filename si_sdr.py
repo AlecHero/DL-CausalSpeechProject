3e1fe76e003282d6fff976c29b0a1548ee58791c
AlecHero <alexander.wittrup@gmail.com>
2024-12-04 23:23:07 +0100

    si_sdr3

--- si_sdr.py
+++ si_sdr.py
@@ -1,6 +1,7 @@
 from torchmetrics.audio import ScaleInvariantSignalDistortionRatio
 from Dataloader.Dataloader import EarsDataset
 import os
+from tqdm import tqdm
 
 blackhole_path = os.getenv('BLACKHOLE')
 if not blackhole_path:
@@ -11,10 +12,15 @@ dataset_VAL = EarsDataset(data_dir=os.path.join(blackhole_path, "EARS-WHAM"), su
 
 noisy_data = []
 clean_data = []
-for i in range(len(dataset_TRN)):
+for i in tqdm(range(len(dataset_TRN))):
     noisy, clean = dataset_TRN[i]
     noisy_data.append(noisy)
     clean_data.append(clean)
 
-si_sdr = ScaleInvariantSignalDistortionRatio()
-si_sdr(noisy_data, clean_data)
+# save noisy and clean data in files called /tmp/noisy_data and /tmp/clean_data in a format that is easily readable by the user
+import numpy as np
+np.save('/tmp/noisy_data', np.array(noisy_data))
+np.save('/tmp/clean_data', np.array(clean_data))
+
+# si_sdr = ScaleInvariantSignalDistortionRatio()
+# si_sdr(noisy_data, clean_data)
